7d62d59bda97ce0cb7488f4595300d8682da6041
kkchara <80607663+kkchara@users.noreply.github.com>
2025-06-22 23:08:14 +0800

    Add initial Arknights themed static page

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// 一个简单的JS，可以在这里添加一些交互，目前只是打印一条信息
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('罗德岛档案页面加载完成！');
-    // 可以在这里添加更多交互功能，比如点击效果、数据加载等
-});
+console.log("罗德岛档案页面已加载.");
+// You can add more interactive JavaScript here later if needed.
+// For now, it just logs a message to the console.
